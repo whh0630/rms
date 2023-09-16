d8206f469781e0025f7ad155de386657f4763d8e
whh0630 <whh0630@naver.com>
2023-09-17 00:35:22 +0900

    create the hello page.

---
+++ app.py
@@ -0,0 +1,14 @@
+# app.py
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+# @app.route('/test')
+# def hello_world():
+    # return render_template('test.html')
+
+if __name__ == '__main__':
+    app.run() 
